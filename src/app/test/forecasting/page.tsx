1a8287f14516a0767dcaf6536e178946f8d936e7
AnhThi92 <anhthi92@propolife.co.jp>
2026-06-04 14:47:02 +0700

    d

--- src/app/test/forecasting/page.tsx
+++ src/app/test/forecasting/page.tsx
@@ -3,9 +3,9 @@ import { notFound } from "next/navigation";
 import { DevForecastingAvailableSourceClient } from "./available-source-client";
 
 export default function DevForecastingPage() {
-  if (process.env.NODE_ENV !== "development") {
-    notFound();
-  }
+  // if (process.env.NODE_ENV !== "development") {
+  //   notFound();
+  // }
 
   return <DevForecastingAvailableSourceClient />;
 }
